72c2c1110b55327e53dfb76a14f34998a3b65523
jaredxiao <jaredxiao@126.com>
2023-06-29 07:40:06 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/build.js
+++ wwwroot/laravel/storage/webpacket_site/build.js
@@ -79,7 +79,10 @@ async function runBuild(envData) {
     // 运行npm run build命令
     return new Promise((resolve, reject) => {
         exec(
-            "npm run build && zip -r ./zipfile/" + NEXT_PUBLIC_OWNER + ".zip",
+            // "npm run build && zip -r ./zipfile/" + NEXT_PUBLIC_OWNER + ".zip",
+            "npm run build && zip -q -r ./zipfile/" +
+                NEXT_PUBLIC_OWNER +
+                ".zip ./out/",
             (error, stdout, stderr) => {
                 if (error) {
                     console.error("Build failed:", error);
