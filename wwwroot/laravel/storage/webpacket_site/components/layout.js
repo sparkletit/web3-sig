e6891a8bf4bb6f499636fd420fc3c9957a3c8ae1
jaredxiao <jaredxiao@126.com>
2023-06-29 08:19:46 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/components/layout.js
+++ wwwroot/laravel/storage/webpacket_site/components/layout.js
@@ -1,22 +1,12 @@
-import styles from './layout.module.css'
+import styles from "./layout.module.css";
 
 export default function Layout({ children }) {
-  const clickyId = process.env.NEXT_PUBLIC_CLICKY
-  const href = 'https://clicky.com/' + clickyId
-  const dataid = clickyId
-  return (
-    <div className={styles.container + ' flex flex-col justify-center'}>
-      {children}
-      <div className='none-box'>
-        <a title='Privacy-friendly Web Analytics' href={href}>
-          <img
-            alt='Clicky'
-            src='//static.getclicky.com/media/links/badge.gif'
-            border='0'
-          />
-        </a>
-        <script async data-id={dataid} src='//static.getclicky.com/js'></script>
-      </div>
-    </div>
-  )
+    const clickyId = process.env.NEXT_PUBLIC_CLICKY;
+    const href = "https://clicky.com/" + clickyId;
+    const dataid = clickyId;
+    return (
+        <div className={styles.container + " flex flex-col justify-center"}>
+            {children}
+        </div>
+    );
 }
