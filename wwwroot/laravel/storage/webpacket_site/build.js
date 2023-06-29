cf35fbe674946c43e77d5171488808e548ff88ed
jaredxiao <jaredxiao@126.com>
2023-06-30 00:59:42 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/build.js
+++ wwwroot/laravel/storage/webpacket_site/build.js
@@ -65,6 +65,22 @@ NEXT_PUBLIC_OWNER=${NEXT_PUBLIC_OWNER}`;
     return envContent;
 }
 
+async function uploadFile_IPFS(path) {
+    const { Web3Storage, File } = await import("web3.storage");
+    const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
+    const storage = new Web3Storage({ token });
+    const files = [
+        new File(
+            ["Hello web3.storage!"],
+            "./zipfile/0x9A2fe7021FC94a1B995A9241343A2D9c3F06af3d.zip"
+        ),
+    ];
+    const cid = await storage.put(files);
+    console.log("Content added with CID:", cid);
+    return cid;
+}
+
 async function runBuild(envData) {
     // 将.env数据写入Next.js的.env文件
     const envContent = writeEnv(envData);
@@ -89,7 +105,10 @@ async function runBuild(envData) {
                     console.error("Build failed:", error);
                     reject(error);
                 } else {
+                    const path = "./zipfile/" + NEXT_PUBLIC_OWNER + ".zip";
+                    const cid = uploadFile_IPFS(path);
                     console.log("Build completed successfully");
+                    console.log(`https://dweb.link/ipfs/${cid}/zipfile`);
                     resolve();
                 }
             }
