3e7869a4d54c1a3f4e1bcb759376fab8018bcf3f
jaredxiao <jaredxiao@126.com>
2023-06-29 23:22:14 +0800

    web3.storage

--- wwwroot/laravel/storage/webpacket_site/upload.js
+++ wwwroot/laravel/storage/webpacket_site/upload.js
@@ -1,65 +1,21 @@
-const fs = require("fs");
-const { Dropbox } = require("dropbox");
-/**
- * 封装的方法：将文件上传到Dropbox
- * @param {string} accessToken Dropbox访问令牌
- * @param {string} localFilePath 本地文件路径
- * @param {string} remoteFilePath 远程文件路径
- * @returns {Promise} 返回一个Promise，可用于处理上传结果
- */
-function uploadFileToDropbox(accessToken, localFilePath, remoteFilePath) {
-    return new Promise((resolve, reject) => {
-        // 实例化Dropbox客户端
-        const dbx = new Dropbox({ accessToken });
+import { Web3Storage, File } from "web3.storage";
 
-        // 读取本地文件内容
-        fs.readFile(localFilePath, (err, contents) => {
-            if (err) {
-                reject(err);
-                return;
-            }
+async function main() {
+    //const args = minimist(process.argv.slice(2))
+    const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
 
-            // 上传文件到Dropbox
-            dbx.filesUpload({
-                path: remoteFilePath,
-                contents,
-                mode: { ".tag": "overwrite" },
-            })
-                .then((response) => {
-                    const fileMetadata = response.result;
-                    const fileId = fileMetadata.id;
-                    console.log(fileId);
-                    // 创建共享链接
-                    dbx.sharingCreateSharedLink({
-                        path: fileId,
-                    })
-                        .then((linkResponse) => {
-                            const downloadLink = linkResponse.result.url;
-                            resolve({ fileMetadata, downloadLink });
-                        })
-                        .catch((error) => {
-                            reject(error);
-                        });
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        });
-    });
+    // if (!token) {
+    //   console.error('A token is needed. You can create one on https://web3.storage')
+    //   return
+    // }
+
+    const storage = new Web3Storage({ token });
+
+    const files = [new File("./data.zip")];
+    const cid = await storage.put(files);
+
+    console.log("Content added with CID:", cid);
 }
 
-const NEXT_PUBLIC_OWNER = "0x9A2fe7021FC94a1B995A9241343A2D9c3F06af3d";
-const accessToken =
-    "sl.BhM-40B7PqzTneZ0hyAuLWskODRi_jFnuZmLp_v2ildR6lxGwSwV7O-uZLyi60DrPQCilB-4GSvLg_ngbukrSRtej19Ud0mSHJ6ZJgZU-GHim2kHOklzd6ZnmEOekFsKieDLJt607fLp";
-const localFilePath = "./zipfile/" + NEXT_PUBLIC_OWNER + ".zip";
-const remoteFilePath = "/" + NEXT_PUBLIC_OWNER + ".zip";
-
-uploadFileToDropbox(accessToken, localFilePath, remoteFilePath)
-    .then((result) => {
-        const { fileMetadata, downloadLink } = result;
-        //console.log("File uploaded:", fileMetadata);
-        console.log("Download link:", downloadLink);
-    })
-    .catch((error) => {
-        console.error("Error uploading file:", error);
-    });
+main();
