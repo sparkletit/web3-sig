b88e0ed9ad66f0c446d98ae6df980020ff980140
jaredxiao <jaredxiao@126.com>
2023-06-30 00:50:20 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/upload.js
+++ wwwroot/laravel/storage/webpacket_site/upload.js
@@ -1,20 +1,15 @@
-import { Web3Storage, File } from "web3.storage";
-
 async function main() {
-    //const args = minimist(process.argv.slice(2))
+    const { Web3Storage, File } = await import("web3.storage");
     const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
-
-    // if (!token) {
-    //   console.error('A token is needed. You can create one on https://web3.storage')
-    //   return
-    // }
-
     const storage = new Web3Storage({ token });
-
-    const files = [new File("./data.zip")];
+    const files = [
+        new File(
+            ["Hello web3.storage!"],
+            "./zipfile/0x9A2fe7021FC94a1B995A9241343A2D9c3F06af3d.zip"
+        ),
+    ];
     const cid = await storage.put(files);
-
     console.log("Content added with CID:", cid);
 }
 
