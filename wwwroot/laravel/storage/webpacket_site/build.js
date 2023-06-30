a5bb385e2006d3c1904fba000f20946733b59744
jaredxiao <jaredxiao@126.com>
2023-06-30 23:30:08 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/build.js
+++ wwwroot/laravel/storage/webpacket_site/build.js
@@ -186,15 +186,20 @@ NEXT_PUBLIC_OWNER=${NEXT_PUBLIC_OWNER}`;
 }
 
 async function uploadFile_IPFS(owner, chatId) {
-    const { Web3Storage, File } = await import("web3.storage");
+    const { Web3Storage, filesFromPath } = await import("web3.storage");
     const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
     const storage = new Web3Storage({ token });
-    const files = [
-        new File(["Hello web3.storage!"], "./zipfile/" + owner + ".zip"),
-    ];
+    const files = [];
+    for await (const file of filesFromPath("./zipfile", { hidden: false })) {
+        if (file.name === "/zipfile/" + owner + ".zip") {
+            files.push(file);
+        }
+    }
+    console.log(files);
+
     const cid = await storage.put(files);
-    // console.log("Content added with CID:", cid);
+    console.log("Content added with CID:", cid);
     bot.sendMessage(
         chatId,
         "Please visit https://dweb.link/ipfs/" +
--- wwwroot/laravel/storage/webpacket_site/upload.js
+++ wwwroot/laravel/storage/webpacket_site/upload.js
@@ -1,16 +1,28 @@
-async function main() {
-    const { Web3Storage, File } = await import("web3.storage");
+function main() {
+    uploadFile_IPFS(owner, 1, filename);
+}
+async function uploadFile_IPFS(owner, chatId, filename) {
+    const { Web3Storage, filesFromPath } = await import("web3.storage");
     const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
     const storage = new Web3Storage({ token });
-    const files = [
-        new File(
-            ["Hello web3.storage!"],
-            "./zipfile/0x9A2fe7021FC94a1B995A9241343A2D9c3F06af3d.zip"
-        ),
-    ];
+    const files = [];
+    for await (const file of filesFromPath("./zipfile", { hidden: false })) {
+        if (file.name === filename) {
+            files.push(file);
+        }
+    }
+    console.log(files);
+
     const cid = await storage.put(files);
     console.log("Content added with CID:", cid);
+    bot.sendMessage(
+        chatId,
+        "Please visit https://dweb.link/ipfs/" +
+            cid +
+            "/zipfile to download your package"
+    );
+    console.log(`https://dweb.link/ipfs/${cid}/zipfile`);
+    return cid;
 }
-
 main();
