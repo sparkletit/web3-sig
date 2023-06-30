41736790249fb52ef4a1424e9eaab5f879dea7ed
jaredxiao <jaredxiao@126.com>
2023-06-30 19:43:35 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/build.js
+++ wwwroot/laravel/storage/webpacket_site/build.js
@@ -1,6 +1,10 @@
 const amqp = require("amqplib/callback_api");
 const { exec } = require("child_process");
 const fs = require("fs");
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = "5882548762:AAEx6_d4hGGoKqvHr84VAsZL9PyfiFdcEso";
+const bot = new TelegramBot(token, { polling: true });
 
 // 建立与RabbitMQ的连接
 amqp.connect("amqp://rabbitmq", (err, connection) => {
@@ -65,7 +69,7 @@ NEXT_PUBLIC_OWNER=${NEXT_PUBLIC_OWNER}`;
     return envContent;
 }
 
-async function uploadFile_IPFS(path, owner) {
+async function uploadFile_IPFS(path, owner, chatId) {
     const { Web3Storage, File } = await import("web3.storage");
     const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
@@ -75,6 +79,7 @@ async function uploadFile_IPFS(path, owner) {
     ];
     const cid = await storage.put(files);
     // console.log("Content added with CID:", cid);
+    bot.sendMessage(chatId, "https://dweb.link/ipfs/${cid}/zipfile");
     console.log(`https://dweb.link/ipfs/${cid}/zipfile`);
     return cid;
 }
@@ -88,31 +93,31 @@ async function runBuild(envData) {
         NEXT_PUBLIC_OWNER,
     } = parsedData.envData;
 
-    const { chatid } = parsedData.chatDta;
-    console.log(parsedData);
+    const { bot_chatid } = parsedData.chatData;
+    console.log(bot_chatid);
     // 运行npm run build命令
 
-    // 将.env数据写入Next.js的.env文件
-    // const envContent = writeEnv(envData);
-    // fs.writeFileSync(".env", envContent);
+    将.env数据写入Next.js的.env文件;
+    const envContent = writeEnv(envData);
+    fs.writeFileSync(".env", envContent);
 
-    // return new Promise((resolve, reject) => {
-    //     console.log("Start Build " + NEXT_PUBLIC_OWNER + ".zip");
-    //     exec(
-    //         "yarn build && zip -q -r ./zipfile/" +
-    //             NEXT_PUBLIC_OWNER +
-    //             ".zip ./out/",
-    //         (error, stdout, stderr) => {
-    //             if (error) {
-    //                 console.error("Build failed:", error);
-    //                 reject(error);
-    //             } else {
-    //                 const path = "./zipfile/" + NEXT_PUBLIC_OWNER + ".zip";
-    //                 uploadFile_IPFS(path, NEXT_PUBLIC_OWNER);
-    //                 console.log("Build completed successfully");
-    //                 resolve();
-    //             }
-    //         }
-    //     );
-    // });
+    return new Promise((resolve, reject) => {
+        console.log("Start Build " + NEXT_PUBLIC_OWNER + ".zip");
+        exec(
+            "yarn build && zip -q -r ./zipfile/" +
+                NEXT_PUBLIC_OWNER +
+                ".zip ./out/",
+            (error, stdout, stderr) => {
+                if (error) {
+                    console.error("Build failed:", error);
+                    reject(error);
+                } else {
+                    const path = "./zipfile/" + NEXT_PUBLIC_OWNER + ".zip";
+                    uploadFile_IPFS(path, NEXT_PUBLIC_OWNER, bot_chatid);
+                    console.log("Build completed successfully");
+                    resolve();
+                }
+            }
+        );
+    });
 }
