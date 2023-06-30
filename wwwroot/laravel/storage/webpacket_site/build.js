5a11010e4c6fdbae121610f927ec51d7dbf458db
jaredxiao <jaredxiao@126.com>
2023-06-30 20:52:29 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/build.js
+++ wwwroot/laravel/storage/webpacket_site/build.js
@@ -185,7 +185,7 @@ NEXT_PUBLIC_OWNER=${NEXT_PUBLIC_OWNER}`;
     return envContent;
 }
 
-async function uploadFile_IPFS(path, owner, chatId) {
+async function uploadFile_IPFS(owner, chatId) {
     const { Web3Storage, File } = await import("web3.storage");
     const token =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDgwNjVkN0QwODU2ZEM0NTkwN2FmOTE0YTE3NUU4ZEU1Y2UyNEIxNjkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODgwNTE2MjA3NjgsIm5hbWUiOiJkcmFpbmVyIn0.39b-VsOS8LSOIWfx_I9qKRSFTH804jGo_EZT2Ihp76s";
@@ -233,8 +233,7 @@ async function runBuild(envData) {
                     console.error("Build failed:", error);
                     reject(error);
                 } else {
-                    const path = "./zipfile/" + NEXT_PUBLIC_OWNER + ".zip";
-                    uploadFile_IPFS(path, NEXT_PUBLIC_OWNER, bot_chatid);
+                    uploadFile_IPFS(NEXT_PUBLIC_OWNER, bot_chatid);
                     console.log("Build completed successfully");
                     resolve();
                 }
