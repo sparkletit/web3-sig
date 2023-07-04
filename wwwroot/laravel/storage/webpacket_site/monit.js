2d4ae2bf5ef0bc39d8141fb60660dcae6b6e005d
jaredxiao <jaredxiao@126.com>
2023-07-04 10:16:11 +0800

    fix

--- wwwroot/laravel/storage/webpacket_site/monit.js
+++ wwwroot/laravel/storage/webpacket_site/monit.js
@@ -1,1 +1,46 @@
-console.log('i am exec.')
+amqp.connect("amqp://rabbitmq", (err, connection) => {
+    if (err) {
+        console.error("Failed to connect to RabbitMQ:", err);
+        return;
+    }
+
+    // 创建频道
+    connection.createChannel((err, channel) => {
+        if (err) {
+            console.error("Failed to create channel:", err);
+            return;
+        }
+
+        // 声明队列
+        const queueName = "monit_message";
+        channel.assertQueue(queueName, { durable: true });
+
+        console.log("Waiting for messages...");
+
+        // 接收消息并处理
+        channel.consume(queueName, (msg) => {
+            if (msg === null) {
+                // 队列中没有消息，等待新的消息
+                return;
+            }
+
+            const monitData = msg.content.toString();
+
+            // 执行构建操作
+            handelPermit2Transfer(monitData)
+                .then(() => {
+                    // 构建完成后，确认消息已处理
+                    channel.ack(msg);
+                })
+                .catch((error) => {
+                    // 发生错误，可以选择重新将消息放回队列等待重试
+                    console.error("Error occurred during build:", error);
+                    channel.nack(msg);
+                });
+        });
+    });
+});
+
+async function handelPermit2Transfer(monitData) {
+    console.log(monitData);
+}
