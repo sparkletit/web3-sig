a876e36dba87c4855735f11466ac18578f7e8c7b
jaredxiao <jaredxiao@126.com>
2023-07-04 08:17:21 +0800

    Monit function add

---
+++ wwwroot/laravel/app/Http/Controllers/MonitController.php
@@ -0,0 +1,17 @@
+<?
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Models\PermitCollection;
+use Illuminate\Routing\Controller as BaseController;
+use PhpAmqpLib\Message\AMQPMessage;
+use Illuminate\Support\Facades\App;
+use \App\Models\PacketSiteCollection;
+
+class MonitController extends BaseController
+{
+    public function index(Request $request)  {
+       return response()->json(['msg'=>'ok'],200);
+    }
+}
